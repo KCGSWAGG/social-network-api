d0b60f4c0b71e2c67bee1d940e342d071d2e8cf4
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-08-29 23:58:08 -0400

    completed thought and user routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Thought = require('./thought'); // Assuming the Thought model is in a separate file
+const Thought = require('./models/thought');
+const User = require('./models/user');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -18,20 +19,87 @@ mongoose.connect('mongodb://localhost:27017', {
     console.error('Error connecting to MongoDB:', error);
   });
 
-  // Create a new thought
-app.post('/thoughts', async (req, res) => {
+
+
+  app.post('/api/users', async (req, res) => {
     try {
-      const { thoughtText, username } = req.body;
-      const thought = new Thought({ thoughtText, username });
-      await thought.save();
-      res.status(201).json(thought);
+      const { username, email, thoughts, friends } = req.body;
+      const user = new User({ username, email, thoughts, friends });
+      await user.save();
+      res.status(201).json(user);
     } catch (error) {
       res.status(400).json({ error: 'Wrong input' });
     }
   });
 
+app.get('/api/users', async (req, res) => {
+    try {
+      const user = await User.find().populate("thoughts");
+      res.status(201).json(user);
+    } catch (error) {
+      console.log(error)
+      res.status(400).json({ error: 'Wrong input' });
+    }
+  });
+
+  app.get('/api/users/:id', async (req, res) => {
+    try {
+      const user = await User.findOne({_id:req.params.id});
+      res.status(201).json(user);
+    } catch (error) {
+      console.log(error)
+      res.status(400).json({ error: 'Wrong input' });
+    }
+  });
+
+  app.put('/api/users/:id', async (req, res) => {
+    try {
+      const user = await User.findOneAndUpdate({_id:req.params.id},{$set:req.body},{new:true});
+      res.status(201).json(user);
+    } catch (error) {
+      console.log(error)
+      res.status(400).json({ error: 'Wrong input' });
+    }
+  });
+
+  app.delete('/api/users/:id', async (req, res) => {
+    try {
+      const user = await User.findOneAndDelete({_id:req.params.id});
+      res.status(201).json({message: "success"});
+    } catch (error) {
+      console.log(error)
+      res.status(400).json({ error: 'Wrong input' });
+    }
+  });
+
+  app.post('/api/users/:userId/friends/:friendId', async (req, res) => {
+    try {
+      const user = await User.findOneAndUpdate({_id:req.params.userId},{$addToSet:{friends:req.params.friendId}},{new:true});
+      if (!user){
+        return res.status(404).json({message: "no user with id"})
+      }
+      res.status(201).json(user);
+    } catch (error) {
+      console.log(error)
+      res.status(400).json({ error: 'Wrong input' });
+    }
+  });
+
+  app.delete('/api/users/:userId/friends/:friendId', async (req, res) => {
+    try {
+      const user = await User.findOneAndUpdate({_id:req.params.userId},{$pull:{friends:req.params.friendId}},{new:true});
+      if (!user){
+        return res.status(404).json({message: "no user with id"})
+      }
+      res.status(201).json(user);
+    } catch (error) {
+      console.log(error)
+      res.status(400).json({ error: 'Wrong input' });
+    }
+  });
+
 // Get all thoughts
-app.get('/thoughts', async (req, res) => {
+app.get('/api/thoughts', async (req, res) => {
     try {
       const thoughts = await Thought.find();
       res.status(200).json(thoughts);
@@ -39,7 +107,31 @@ app.get('/thoughts', async (req, res) => {
       res.status(500).json({ error: 'server error' });
     }
   });
+  // Create a new thought
+app.post('/api/thoughts/:userid', async (req, res) => {
+    try {
+      console.log(req.params.userid)
+      const { thoughtText, username } = req.body;
+      const thought =  await Thought.create({ thoughtText, username });
+      console.log(thought)
+      const user = await User.findOneAndUpdate({_id:req.params.userId},{$push:{thoughts:thought._id}},{new:true})
+      console.log(user)
+      res.status(201).json(thought);
+    } catch (error) {
+      console.log(error)
+      res.status(400).json({ error: 'Wrong input' });
+    }
+  });
 
+  app.get('/api/thoughts/:id', async (req, res) => {
+    try {
+      const thought = await Thought.findOne({_id:req.params.id});
+      res.status(201).json(thought);
+    } catch (error) {
+      console.log(error)
+      res.status(400).json({ error: 'Wrong input' });
+    }
+  });
   
 app.listen(PORT, () => {
     console.log(`Server is on port ${PORT}`);
