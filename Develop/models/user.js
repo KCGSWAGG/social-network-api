25f501dad62d0a5fd0c8e816a9da2e87a9d55e7d
KCGSWAGG <Tyrealworley999@yahoo.com>
2023-09-05 20:06:07 -0400

    get user by id route

--- Develop/models/user.js
+++ Develop/models/user.js
@@ -15,11 +15,11 @@ const userSchema = new mongoose.Schema({
   },
   thoughts: [{
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'Thought', // Assuming you have a Thought model
+    ref: 'Thought', 
   }],
   friends: [{
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', // Self-reference to the User model
+    ref: 'User', 
   }],
 });
 
